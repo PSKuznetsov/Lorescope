17dc68921923635fc3f40cad15e9fb5bfb793853
Paul <paulrussian@me.com>
2015-10-16 01:35:51 +0300

    Initial Commit

---
+++ lorescope/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lorescope
+//
+//  Created by Paul Kuznetsov on 16/10/15.
+//  Copyright © 2015 Paul Kuznetsov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ lorescope/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  lorescope
+//
+//  Created by Paul Kuznetsov on 16/10/15.
+//  Copyright © 2015 Paul Kuznetsov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
